1c3c1278a24e93927fcd984780d7ae1f650a1d35
lalomax <lalomax365@gmail.com>
2023-01-26 20:18:18 -0400

    downgrade react 18 to 17

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
-import {createRoot } from "react-dom/client"
+import React from "react"
+import ReactDOM from "react-dom"
 
 
 import App from "./App"
 import './index.css'
 
-const root = createRoot(document.getElementById("root"))
-root.render(<App />, )
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
